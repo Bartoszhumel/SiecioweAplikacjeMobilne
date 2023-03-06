6c52b0e257e1f850674e411f03ca941942680054
Bartoszhumel <79085432+Bartoszhumel@users.noreply.github.com>
2023-03-06 15:49:35 +0100

    Update server.js

--- lab1/source/server.js
+++ lab1/source/server.js
@@ -3,12 +3,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
     video= URLSearchParams.get("videoFile")
     audio = URLSearchParams.get("audioFile")
     const video1 = document.createElement('video');
     video1.src = video;
-    res.send(<video>  <source src=video type="video/mp4"></video>)
+    res.send(<video>  <source src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"></source> type="video/mp4"></video>)
 
 })
 
