c53b36315874eb89bd37a203e443eff0245c7efa
Bartoszhumel <79085432+Bartoszhumel@users.noreply.github.com>
2023-03-20 15:08:00 +0100

    Dodanie obsługi posterImage

--- lab2/source/server.js
+++ lab2/source/server.js
@@ -9,14 +9,24 @@ app.get('/', (req, res) => {
   const queryObject = url.parse(req.url, true).query;
   const videoFile = queryObject.videoFile;
   const audioFile = queryObject.audioFile;
+  const imgFile = queryObject.posterFile;
   let player;
 
+  if(videoFile&&audioFile)
+  {
+    player = `<video id="videoPlayer" src="${videoFile}" controls></video>
+              <audio id="audioPlayer" src="${audioFile}" controls></audio>`;
+  }
   if (videoFile) {
     player = `<video id="videoPlayer" src="${videoFile}" controls></video>`;
   } else if (audioFile) {
     player = `<audio id="audioPlayer" src="${audioFile}" controls></audio>`;
   } else {
     player = 'No media file specified.';
+  }
+  let image;
+  if(imgFile){
+    image = `<img id="posterImage" src="${imgFile}"></img>`
   }
 
   const html = `
@@ -26,6 +36,7 @@ app.get('/', (req, res) => {
       </head>
       <body>
         ${player}
+        ${image}
       </body>
     </html>
   `;
